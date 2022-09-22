cc0f816c7b480175f37cb93d3e391f9c4db2c47d
Gukss <dongkuk97@naver.com>
2022-09-22 13:20:27 +0900

    algo

--- JavaAlgorithm/src/_20220921/Bj_12865_knapsack.java
+++ JavaAlgorithm/src/_20220921/Bj_12865_knapsack.java
@@ -8,6 +8,19 @@ import java.util.StringTokenizer;
 public class Bj_12865_knapsack {
     static int N,K;
     static int[][] s;
+    static int[][] data;
+
+    public static void knap(){
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) { //임시 배낭 용량
+                if(s[i][0] > j){
+                    data[i][j] = data[i-1][j];
+                }else{
+                    data[i][j] = Math.max(data[i-1][j], data[i-1][j-s[i][0]]+s[i][1]);
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,12 +28,16 @@ public class Bj_12865_knapsack {
 
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        s = new int[N][2];
+        s = new int[N+1][2];
+        data = new int[N+1][100001]; //N과 K 모두 1부터 시작
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            s[i][0] = Integer.parseInt(st.nextToken());
-            s[i][1] = Integer.parseInt(st.nextToken());
+            s[i][0] = Integer.parseInt(st.nextToken()); //무게w
+            s[i][1] = Integer.parseInt(st.nextToken()); //가치v
         }
+
+        knap();
+        System.out.println(data[N][K]);
     }
 }
