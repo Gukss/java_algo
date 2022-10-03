5acc7f10ec60319193ebbb168821eb9439bc675d
Gukss <76477531+Gukss@users.noreply.github.com>
2022-10-03 10:41:16 +0900

    [Silver III] Title: 이항 계수 2, Time: 96 ms, Memory: 15520 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N,K;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        dp = new int[N+1][K+1];
+
+        //f(n,k) = f(n-1,k)+f(n-1,k-1)
+        for (int i = 0; i <= N; i++) {
+            for (int j = 0; j <= Math.min(i,K); j++) {
+                if(i==j || j==0){
+                    dp[i][j] = 1;
+                }else{
+                    dp[i][j] = (dp[i-1][j]+dp[i-1][j-1]) % 10007;
+                }
+            }
+        }
+        System.out.println(dp[N][K]);
+    }
+}
