f7a82880a57bab29fc8e73ebd32a937557af7451
Gukss <76477531+Gukss@users.noreply.github.com>
2022-10-05 23:28:19 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 76 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean[] v = new boolean[31];
+        for (int i = 0; i < 28; i++) {
+            int idx = Integer.parseInt(br.readLine());
+            v[idx] = true;
+        }
+        for (int i = 1; i < 31; i++) {
+            if(!v[i]){
+                System.out.println(i);
+            }
+        }
+    }
+}
