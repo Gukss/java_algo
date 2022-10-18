33867c12cdbec10e744715efc01660f94e7dc50a
Gukss <dongkuk97@naver.com>
2022-10-18 23:57:08 +0900

    algo

--- JavaAlgorithm/src/_20221016/Bj_16235_tree.java
+++ JavaAlgorithm/src/_20221016/Bj_16235_tree.java
@@ -1,7 +1,5 @@
 package _20221016;
 
-import com.sun.source.tree.Tree;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,33 +8,31 @@ import java.util.*;
 public class Bj_16235_tree {
     static int N,M,K;
     static int[][] map, feed;
-    static List<Tree> list;
-    static Queue<Tree> deadqu;
+    static Deque<Tree> dq;
+    static ArrayList<Tree> deadlist;
     static int[] dr = {-1, -1, 0, 1, 1, 1, 0, -1}; //위 부터 시계방향
     static int[] dc = {0, 1, 1, 1, 0, -1, -1, -1}; //위 부터 시계방향
     public static void spring(){
-        list.sort(new Comparator<Tree>() {
-            @Override
-            public int compare(Tree o1, Tree o2) {
-                return o1.age - o2.age;
-            }
-        });
-        for(Tree cur:list){
+        deadlist = new ArrayList<>();
+        for(Tree cur: dq){
             int r = cur.r;
             int c = cur.c;
             if(map[r][c] - cur.age >= 0){ //나이만큼 양분을 먹을수 있을 때
                 map[r][c] -= cur.age; //양분 먹고
                 cur.age += 1; //나이 +1
+                if(cur.age % 5 == 0){
+                    cur.sun = 1;
+                }
             }else{ //양분은 먹을 수 없을 때
-                deadqu.add(cur);
+                deadlist.add(cur);
             }
         }
     }
 
     public static void summer(){
-        while(!deadqu.isEmpty()){
-            Tree cur = deadqu.poll();
-            list.remove(cur); //죽은 나무 list에서 빼기
+        int size = deadlist.size();
+        for (Tree cur: deadlist){
+            dq.remove(cur); //죽은 나무 list에서 빼기
             int r = cur.r;
             int c = cur.c;
             int age = cur.age;
@@ -46,18 +42,25 @@ public class Bj_16235_tree {
     }
 
     public static void autumn(){
-        for(Tree cur: list){
-            if(cur.age % 5 == 0){ //나무의 나이가 5의 배수일 때
-                int sr = cur.r;
-                int sc = cur.c;
-                for (int i = 0; i < 8; i++) {
-                    int nr = sr + dr[i];
-                    int nc = sc + dc[i];
-                    if(nr>=1&&nr<=N&&nc>=1&&nc<=N){ //경계
-                        list.add(new Tree(nr, nc, 1)); //나이가 1인 나무 생성
+        List<Tree> templist = new ArrayList<>();
+        for(Tree cur: dq){
+            if(cur.age % 5==0){ //나무의 나이가 5의 배수일 때
+                if(cur.sun == 1){
+                    int sr = cur.r;
+                    int sc = cur.c;
+                    cur.sun = 0;
+                    for (int i = 0; i < 8; i++) {
+                        int nr = sr + dr[i];
+                        int nc = sc + dc[i];
+                        if(nr>=1&&nr<=N&&nc>=1&&nc<=N){ //경계
+                            templist.add(new Tree(nr, nc, 1,0)); //나이가 1인 나무 생성
+                        }
                     }
                 }
             }
+        }
+        for(Tree cur: templist){
+            dq.addFirst(cur);
         }
     }
 
@@ -81,39 +84,39 @@ public class Bj_16235_tree {
         feed = new int[N+1][N+1]; //r,c는 1부터 시작한다.
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                feed[i][j] = map[i][j];
+            for (int j = 1; j <= N; j++) { //처음 양분은 모두 5다. => 기본 양분 5에 += 5가 아니다.
+                feed[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] += 5; //기본 양분이 5있다.
             }
         }
 
-        list = new ArrayList<>();
+        dq = new ArrayDeque<>();
         for (int i = 0; i < M; i++) {
             int x,y,z;
             st = new StringTokenizer(br.readLine());
             x = Integer.parseInt(st.nextToken());
             y = Integer.parseInt(st.nextToken());
             z = Integer.parseInt(st.nextToken());
-            list.add(new Tree(x,y,z));
+            dq.add(new Tree(x,y,z,0));
         }
 
-        deadqu = new LinkedList<>();
         for (int i = 0; i < K; i++) {
             spring();
             summer();
             autumn();
             winter();
         }
-        System.out.println(list.size());
+        System.out.println(dq.size());
     }
 
     public static class Tree{
-        int r,c,age;
+        int r,c,age,sun;
 
-        public Tree(int r, int c, int age) {
+        public Tree(int r, int c, int age, int sun) {
             this.r = r;
             this.c = c;
             this.age = age;
+            this.sun = sun;
         }
     }
 }
