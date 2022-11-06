59f62f58e9eb40bb1d4cd16b4b20acd268bd10ae
Gukss <76477531+Gukss@users.noreply.github.com>
2022-11-06 09:44:28 +0900

    [Silver IV] Title: 30, Time: 156 ms, Memory: 20116 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -2,9 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
 
 public class Main {
     static String str;
@@ -17,14 +14,16 @@ public class Main {
         Arrays.sort(arr);
         int sum = 0;
         String result = "";
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < arr.length; i++) {
             sum += Integer.parseInt(String.valueOf(arr[i]));
+            sb.append(arr[i]);
         }
-        for (int i = arr.length-1; i >= 0; i--) {
-            result += arr[i];
-        }
+//        for (int i = arr.length-1; i >= 0; i--) {
+//            result += arr[i];
+//        }
         if(sum % 3 == 0 && arr[0]=='0'){
-            System.out.println(result);
+            System.out.println(sb.reverse());
         }else{
             System.out.println(-1);
         }
