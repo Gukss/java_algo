a00cdbaa0a9eecdad8e5036984f430d460e29ed3
Gukss <dongkuk97@naver.com>
2022-12-14 10:51:57 +0900

    Update 신고 결과 받기.java

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -7,7 +7,7 @@ class Solution {
     //신고당한 개수가 k보다 큰 banList[]를 반복하면서 answer[] += 1 해준다.
     
     public static int K, size;
-    public static Set[] banList, setList;
+    public static Set[] banList;//, setList;
     public static String[] idList;
     public static String[] Report;
     public static Map<String, Integer> mapper;
@@ -19,7 +19,7 @@ class Solution {
             String one = st.nextToken(); //one->two 이 방향으로 신고한다.
             String two = st.nextToken();
             banList[mapper.get(two)].add(one); //내가 신고 당한것 => banList[i] 조회하면 idx가 누구한테 신고당했는지 적혀있다.
-            setList[mapper.get(one)].add(two); //내가 신고 한 것 => 내가 신고한 사람을 저장해서 결과 출력할 때 사용하려 했으나 
+            // setList[mapper.get(one)].add(two); //내가 신고 한 것 => 내가 신고한 사람을 저장해서 결과 출력할 때 사용하려 했으나 
         }
     }
     
@@ -41,11 +41,11 @@ class Solution {
         size = idList.length; //유저 숫자
         result = new int[size]; //메일 받을 횟수 저장할 배열
         banList = new HashSet[size]; //idx에 해당하는 유저가 신고 당한 것, 신고를 한 유저가 저장돼있다. => 신고당한 횟수를 알 수 있다.
-        setList = new HashSet[size]; //idx에 해당하는 유저가 신고 한 것, 신고를 당한 유저를 저장한다. => 사용하지 않았다.
+        // setList = new HashSet[size]; //idx에 해당하는 유저가 신고 한 것, 신고를 당한 유저를 저장한다. => 사용하지 않았다.
         mapper = new HashMap<>(); //String을 int로 mapping한다.
         for(int i=0;i<size;i++){ //banList[]에 set을 초기화하고, 매퍼도 초기화한다.
             banList[i] = new HashSet<String>();
-            setList[i] = new HashSet<String>();
+            // setList[i] = new HashSet<String>();
             mapper.put(idList[i], i);
         }
         
