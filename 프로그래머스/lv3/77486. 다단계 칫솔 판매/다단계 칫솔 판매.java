5291277d0d0638705a10d1036941a9895e4865f6
Gukss <dongkuk97@naver.com>
2022-12-18 13:25:53 +0900

    Update 다단계 칫솔 판매.java

--- 프로그래머스/lv3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/lv3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 class Solution {
     //referral를 사용해서 노드끼리 연결 한다. => 인접 리스트로 만든다.
-    //amount 마지막 부터 올라가면서 결과를 만든다.
     
     public static String[] Enroll, Referral, Seller;
     public static int[] Amount;
@@ -12,18 +11,18 @@ class Solution {
     
     public static void makeList(String name, int idx){
         if(!name.equals("-")){ //제일 위가 아니면
-            listArr[idx].add(name); //밑에서 부터 자기 부모의 이름을 가지고 있다.
+            listArr[idx].add(name); //밑에서 부터 자기 부모의 이름을 가지고 있다. 부모노드는 하나밖에 없다.
         }
     }
     
     public static void sell(String name, int earn){
-        int parent = earn/10;
-        int mine = earn-parent;
-        result[mapper.get(name)] += mine;
-        if(listArr[mapper.get(name)].isEmpty() || parent==0){
+        int parent = earn/10; //부모에게 줄 10%
+        int mine = earn-parent; //내가 가질 수익
+        result[mapper.get(name)] += mine; //내 수익을 더한다.
+        if(listArr[mapper.get(name)].isEmpty() || parent==0){ //parent가 0이면 더이상 재귀호출할 이유가 없다.=> 백트래킹 || 최상단 노드일 때
             return;
         }
-        sell(listArr[mapper.get(name)].get(0).toString(), parent);
+        sell(listArr[mapper.get(name)].get(0).toString(), parent); //부모노드의 부모에게 10%를 준다.
     }
     
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
@@ -35,16 +34,16 @@ class Solution {
         mapper = new HashMap<>();
         result = new int[Enroll.length];
         
-        for(int i=0;i<Enroll.length;i++){
+        for(int i=0;i<Enroll.length;i++){ //{이름: idx}인 mapper, listArr초기화
             listArr[i] = new ArrayList<String>();
             mapper.put(Enroll[i], i);
         }
         
-        for(int i=0;i<Referral.length;i++){
+        for(int i=0;i<Referral.length;i++){ //인접 리스트를 만든다. 밑에서 부터 자기 부모의 이름을 가지고 있다.
             makeList(Referral[i], i);
         }
         
-        for(int i=0;i<Seller.length;i++){
+        for(int i=0;i<Seller.length;i++){ //수익을 계산한다.
             sell(Seller[i], Amount[i]*100);
         }
         
@@ -52,3 +51,5 @@ class Solution {
         return answer;
     }
 }
+
+//2시간
