f76fdffe7bbaa74edfc637ccdc8bb36a10f83ffc
Gukss <dongkuk97@naver.com>
2024-03-14 21:51:29 +0900

    [level 1] Title: 인기있는 아이스크림, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/133024. 인기있는 아이스크림/인기있는 아이스크림.sql
@@ -0,0 +1,19 @@
+-- 코드를 입력하세요
+select flavor 
+from first_half
+order by total_order desc, shipment_id asc;
+
+
+
+
+
+
+
+
+
+
+
+#SELECT flavor
+#from first_half
+#group by flavor
+#order by sum(total_order) desc, shipment_id asc
