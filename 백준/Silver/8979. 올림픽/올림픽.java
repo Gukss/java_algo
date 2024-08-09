aff2cce2923b2e80f7ccbf84071421bb4c2d49dd
Gukss <dongkuk97@naver.com>
2024-08-09 22:13:51 +0900

    [Silver V] Title: 올림픽, Time: 96 ms, Memory: 12116 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -18,18 +18,18 @@ public class Main {
         input[n-1][j] = Integer.parseInt(st.nextToken());
       }
     }
-    int result = 0;
-    boolean[] v = new boolean[N];
-    for(int i=0;i<=2;i++){
-      for(int j=0;j<N;j++){
-        if(j==K-1){
-          continue;
-        }
-        if(input[K-1][i] < input[j][i] && !v[j]){
+    int result = 1;
+      for(int i=0;i<N;i++){
+        if(input[K-1][0] < input[i][0]){
           result += 1;
-          v[j] = true;
+        }else if((input[K-1][0] == input[i][0]) && (input[K-1][1] < input[i][1])){
+          result += 1;
+        }else if((input[K-1][0] == input[i][0]) && (input[K-1][1] == input[i][1]) && (input[K-1][2] < input[i][2])){
+          result += 1;
         }
       }
+    if(result == 0){
+      result = 1;
     }
     System.out.println(result);
   }
